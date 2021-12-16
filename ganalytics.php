de560f1e582b72253d20f742264d74768f709e5e
Petr Hučík <petr@getdatakick.com>
2021-12-16 08:43:48 +0100

    Release version 3.4.0

--- ganalytics.php
+++ ganalytics.php
@@ -52,7 +52,7 @@ class Ganalytics extends Module
     {
         $this->name = 'ganalytics';
         $this->tab = 'analytics_stats';
-        $this->version = '3.3.6';
+        $this->version = '3.4.0';
         $this->author = 'thirty bees';
         $this->bootstrap = true;
 
